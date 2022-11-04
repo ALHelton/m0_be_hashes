3fa267eaa45ead11d94f8ffbfe5bcf7561d0eebd
Andra Helton <andrahelton@gmail.com>
2022-11-03 20:49:19 -0600

    Add answers to hashes 2 assignment

--- hashes_2.rb
+++ hashes_2.rb
@@ -1,38 +1,36 @@
 # 1- Using the following Hash
 
-states = {
-  "Oregon" => "OR",
-  "Alabama" => "AL",
-  "New Jersey" => "NJ",
-  "Colorado" => "CO"
-}
+# states = {
+#   "Oregon" => "OR",
+#   "Alabama" => "AL",
+#   "New Jersey" => "NJ",
+#   "Colorado" => "CO"
+# }
 
-# Use the each method to iterate through it and print out a sentence 
-# stating each state name and abbreviation
-#
-#
-#
+# # Use the each method to iterate through it and print out a sentence 
+# # stating each state name and abbreviation
+
+# p states.each
 
 
 # ======================================
 # 2- Using the following Hash
 
-birthdays = {
-  "Cindy" => "June 10",
-  "Rigo" => "March 31",
-  "Amos" => "July 21",
-  "Jeff" => "February 12"
-}
+# birthdays = {
+#   "Cindy" => "June 10",
+#   "Rigo" => "March 31",
+#   "Amos" => "July 21",
+#   "Jeff" => "February 12"
+# }
 
-# Use the each method to iterate through it and print out a sentence 
-# stating each friends name and birthday
-#
-#
-#
+# # # Use the each method to iterate through it and print out a sentence 
+# # # stating each friends name and birthday
+
+# p birthdays.each
 
 
-# ======================================
-# 3- Using the following Hash
+# # ======================================
+# # 3- Using the following Hash
 
 login_statuses = {
   "Cindy" => true,
@@ -41,8 +39,11 @@ login_statuses = {
   "Jeff" => true
 }
 
-# Use the each method to iterate through it and print ONLY the names 
-# of friends who are "logged in"
-#
-#
-#
+# # Use the each method to iterate through it and print ONLY the names 
+# # of friends who are "logged in"
+
+login_statuses.each do |name, logged|
+  if logged == true
+    p name
+  end
+end
